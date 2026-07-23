db0c8dcdfe84f05b9607e287db588280cb08f8cd
5 gczy <2565674734@qq.com>
2026-07-23 10:00:11 +0800

    吐槽板只存内存问题

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,12 +1,8 @@
 import { Redis } from "@upstash/redis";
 
-// Upstash Redis 客户端
-// 需要在 .env.local 或 Vercel 环境变量中配置：
-// UPSTASH_REDIS_REST_URL=xxx
-// UPSTASH_REDIS_REST_TOKEN=xxx
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL || "",
-  token: process.env.UPSTASH_REDIS_REST_TOKEN || "",
+  url: process.env.UPSTASH_REDIS_REST_URL?.replace(/"/g, "") || "",
+  token: process.env.UPSTASH_REDIS_REST_TOKEN?.replace(/"/g, "") || "",
 });
 
 export default redis;
